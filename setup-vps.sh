913863ffb972c8c9e5fa60ed24f7a1a1b5222a92
Carl-Gerhard Lindesvärd <lindesvard@gmail.com>
2018-03-25 17:24:17 +0200

    added swapmemoery

--- setup-vps.sh
+++ setup-vps.sh
@@ -27,3 +27,32 @@ service ssh restart
 
 # Create a SSH key
 ssh-keygen -f $HOME/.ssh/id_rsa -t rsa -N ''
+
+# Confirm you have no swap
+sudo swapon -s
+ 
+# Allocate 4GB (or more if you wish) in /swapfile
+sudo fallocate -l 4G /swapfile
+ 
+# Make it secure
+sudo chmod 600 /swapfile
+ls -lh /swapfile
+ 
+# Activate it
+sudo mkswap /swapfile
+sudo swapon /swapfile
+ 
+# Confirm again there's indeed more memory now
+free -m
+sudo swapon -s
+ 
+# Configure fstab to use swap when instance restart
+sudo nano /etc/fstab
+ 
+# Add this line to /etc/fstab, save and exit
+/swapfile   none    swap    sw    0   0
+ 
+# Change swappiness to 10, so that swap is used only when 10% RAM is unused
+# The default is too high at 60
+echo 10 | sudo tee /proc/sys/vm/swappiness
+echo vm.swappiness = 10 | sudo tee -a /etc/sysctl.conf
