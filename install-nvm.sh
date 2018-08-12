3dc567f2209c1d0e350116d9853609f192c4b13f
Carl-Gerhard Lindesvärd <lindesvard@gmail.com>
2018-08-12 15:20:49 +0200

    Update install-nvm.sh

--- install-nvm.sh
+++ install-nvm.sh
@@ -3,8 +3,11 @@
 NODE_VERSION=9.11.2
 NVM_VERSION=0.33.11
 
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
 sudo apt-get update -y
-sudo apt-get install -y build-essential libssl-dev
+sudo apt-get install -y build-essential libssl-dev yarn
 
 curl -sL https://raw.githubusercontent.com/creationix/nvm/v$NVM_VERSION/install.sh -o install_nvm.sh
 
