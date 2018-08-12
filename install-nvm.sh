5993931e57272e65d8f91f80e697881bf47ff4b6
Carl-Gerhard Lindesvärd <lindesvard@gmail.com>
2018-08-12 15:08:19 +0200

    Update install-nvm.sh

--- install-nvm.sh
+++ install-nvm.sh
@@ -10,7 +10,12 @@ curl -sL https://raw.githubusercontent.com/creationix/nvm/v$NVM_VERSION/install.
 
 bash install_nvm.sh
 
-echo "export NVM_DIR=\"\$HOME/.nvm\"\n[ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\"  # This loads nvm\n[ -s \"\$NVM_DIR/bash_completion\" ] && \. \"\$NVM_DIR/bash_completion\"  # This loads nvm bash_completion\nexport NODE_ENV=production\n\n" | cat - .bashrc > .temp.bachrc && mv .temp.bachrc .bashrc
+echo 'export NVM_DIR="$HOME/.nvm"' >> .bashrc.tmp
+echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' >> .bashrc.tmp
+echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >> .bashrc.tmp
+cat .bachrc >> .bachrc.tmp
+mv .bashrc .bashrc.old 
+mv .bashrc.tmp .bashrc
 
 source ~/.bashrc
 
